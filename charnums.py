4d821b261b4e263386f5210f26d5b611415e7d1f
Michael Howitz <mac@gmx.net>
2020-04-19 14:17:00 +0200

    Make flake8 happy.

--- charnums.py
+++ charnums.py
@@ -2,11 +2,12 @@ def _convert(input):
     """Bereite `input` für `charnums` vor."""
     no_space = ''
     input = input.lower()
-    
+
     for x in input:
         if not x.isspace():
             no_space += x
     return no_space
+
 
 def charnums(string):
     """Konvertiere String in Folge von Buchstabenpoitinnen."""
